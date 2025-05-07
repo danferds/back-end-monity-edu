4e3ae559b7a30c7903e65009ad41872ece0d8cd2
Daniel Fernandes <danielferdsc@gmail.com>
2025-05-07 10:07:09 -0300

    Configuração inicial das dependências.

---
+++ src/main/java/com/pi2/monity_edu/MonityEduApplication.java
@@ -0,0 +1,13 @@
+package com.pi2.monity_edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonityEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonityEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi2/monity_edu/MonityEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi2.monity_edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonityEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
